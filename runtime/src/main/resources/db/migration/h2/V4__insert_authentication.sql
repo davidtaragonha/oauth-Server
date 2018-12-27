df8a416478b47f22a23ba8ba9105595f2eb52806
alonsotd <david.alonso.da1@roche.com>
2018-12-27 15:45:08 +0100

    Created client_credentials and use only one encoder

--- runtime/src/main/resources/db/migration/h2/V4__insert_authentication.sql
+++ runtime/src/main/resources/db/migration/h2/V4__insert_authentication.sql
@@ -5,9 +5,14 @@ INSERT INTO OAUTH_CLIENT_DETAILS(CLIENT_ID, CLIENT_SECRET)
 INSERT INTO OAUTH_CLIENT_DETAILS(CLIENT_ID, RESOURCE_IDS, CLIENT_SECRET, SCOPE, AUTHORIZED_GRANT_TYPES, AUTHORITIES, ACCESS_TOKEN_VALIDITY, REFRESH_TOKEN_VALIDITY)
 	VALUES ('my.app', 'iam-resource-server-rest-api,result-resource-server-rest-api',
 	/*spring-security-oauth2-read-write-client-password1234*/'$2a$04$soeOR.QFmClXeFIrhJVLWOQxfHjsJLSpWrU1iGxcMGdu.a5hvfY4W',
-	'iam.read,iam.write,result.read,result.write', 'password,authorization_code,refresh_token,implicit', 'USER', 10800, 2592000);
+	'iam.read,iam.write,result.read,result.write', 'password,authorization_code,refresh_token,implicit', NULL, 10800, 2592000);
+
+INSERT INTO OAUTH_CLIENT_DETAILS(CLIENT_ID, RESOURCE_IDS, CLIENT_SECRET, SCOPE, AUTHORIZED_GRANT_TYPES, AUTHORITIES, ACCESS_TOKEN_VALIDITY, REFRESH_TOKEN_VALIDITY)
+	VALUES ('acl.my.app', 'result-resource-server-rest-api',
+	/*spring-security-oauth2-read-client-password1234*/'$2a$04$WGq2P9egiOYoOFemBRfsiO9qTcyJtNRnPKNBl5tokP7IP.eZn93km',
+	'result.read,result.write', 'client_credentials,refresh_token', NULL, 10800, 2592000);
 
 INSERT INTO OAUTH_CLIENT_DETAILS(CLIENT_ID, RESOURCE_IDS, CLIENT_SECRET, SCOPE, AUTHORIZED_GRANT_TYPES, AUTHORITIES, ACCESS_TOKEN_VALIDITY, REFRESH_TOKEN_VALIDITY)
 	VALUES ('sergas.xunta.de.galicia.com', 'iam-resource-server-rest-api',
 	/*spring-security-oauth2-read-client-password1234*/'$2a$04$WGq2P9egiOYoOFemBRfsiO9qTcyJtNRnPKNBl5tokP7IP.eZn93km',
-	'iam.read', 'password,authorization_code,refresh_token,implicit', 'USER', 10800, 2592000);
+	'iam.read', 'password,authorization_code,refresh_token,implicit', NULL, 10800, 2592000);
